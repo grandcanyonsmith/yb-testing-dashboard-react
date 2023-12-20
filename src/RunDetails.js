20b13e4e0a2e8c1d9ee75aaac8989afd51158e8d
Canyon Smith <canyon.smith@octanner.com>
2023-12-19 17:13:36 -0700

    Update RunDetails.js

--- src/RunDetails.js
+++ src/RunDetails.js
@@ -2,71 +2,90 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { ChevronRightIcon } from '@heroicons/react/solid';
 import { Transition } from '@headlessui/react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+const STATUS_COLORS = {
+  'Passed': 'bg-green-500',
+  'Failed': 'bg-red-500',
+  'Default': 'bg-gray-500',
+};
+
+const fetchRunData = async (testName) => {
+  const url = "https://m3safcz3boalyw7x7dl4qig3wq0tucft.lambda-url.us-west-2.on.aws/";
+  const data = { "testName": testName };
+  const headers = { 'Content-type': 'application/json' };
+
+  try {
+    const response = await axios.post(url, data, { headers });
+    return { data: response.data, error: null };
+  } catch (error) {
+    return { data: null, error: 'There was an error fetching the data. Please try again later.' };
+  }
+};
+
+const DashboardHeader = ({ selectedRun, onViewCodeClick }) => {
+  if (!selectedRun) return null;
+
+  return (
+    <header className="border-b border-gray-700 bg-gray-800 pl-6 py-5">
+      <div className="lg:flex lg:items-center lg:justify-between bg-gray-800 px-4 py-5 sm:px-6">
+        <div className="min-w-0 flex-1">
+          <h2 className="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
+            {selectedRun.formattedTestName}
+          </h2>
+          <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+            <div className="mt-2 flex items-center text-sm text-gray-300">Team: {selectedRun.team}</div>
+            <div className="mt-2 flex items-center text-sm text-gray-300">Status: {selectedRun.status}</div>
+            <div className="mt-2 flex items-center text-sm text-gray-300">ID: {selectedRun.id}</div>
+          </div>
+        </div>
+        <div className="mt-5 flex lg:ml-4 lg:mt-0">
+          <button
+            className="inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400"
+            onClick={onViewCodeClick}
+          >
+            View Code
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 const TestDashboard = () => {
   const [previousRunsData, setPreviousRunsData] = useState([]);
   const [selectedRun, setSelectedRun] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-
-  const statusColors = {
-    'Passed': 'bg-green-500',
-    'Failed': 'bg-red-500',
-  };
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
-    fetchPreviousRuns();
-  }, []);
-
-  const fetchPreviousRuns = async () => {
-    setLoading(true);
-    const testName = new URLSearchParams(window.location.search).get('testName');
-    const url = "https://m3safcz3boalyw7x7dl4qig3wq0tucft.lambda-url.us-west-2.on.aws/";
-    const data = { "testName": testName };
-    const headers = { 'Content-type': 'application/json' };
-
-    try {
-      const response = await axios.post(url, data, { headers: headers });
-      setPreviousRunsData(response.data);
-      setSelectedRun(response.data[0]);
+    const initiateFetch = async () => {
+      setLoading(true);
+      const testName = searchParams.get('testName');
+      const { data, error } = await fetchRunData(testName);
+      setPreviousRunsData(data || []);
+      setSelectedRun(data?.[0] || null);
+      setError(error);
       setLoading(false);
-    } catch (error) {
-      setError('There was an error fetching the data. Please try again later.');
-      setLoading(false);
+    };
+
+    initiateFetch();
+  }, [searchParams]);
+
+
+  const getStatusColor = (status) => STATUS_COLORS[status] || STATUS_COLORS.Default;
+
+  const handleViewCodeClick = () => {
+    if (selectedRun) {
+      navigate(`/viewCode?testName=${encodeURIComponent(selectedRun.testName)}`);
     }
-  }
-
-  const getStatusColor = (status) => {
-    return statusColors[status] || 'bg-gray-500';
-  }
-
-  const navigate = useNavigate();
+  };
 
   return (
     <div className="bg-gray-900 text-gray-200">
-      <header className="border-b border-gray-700 bg-gray-800 pl-6 py-5">
-        {selectedRun && (
-          <div className="lg:flex lg:items-center lg:justify-between bg-gray-800 px-4 py-5 sm:px-6">
-            <div className="min-w-0 flex-1">
-              <h2 className="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">{selectedRun.formattedTestName}</h2>
-              <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-                <div className="mt-2 flex items-center text-sm text-gray-300">Team: {selectedRun.team}</div>
-                <div className="mt-2 flex items-center text-sm text-gray-300">Status: {selectedRun.status}</div>
-                <div className="mt-2 flex items-center text-sm text-gray-300">ID: {selectedRun.id}</div>
-              </div>
-            </div>
-            <div className="mt-5 flex lg:ml-4 lg:mt-0">
-              <button 
-                className="inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400"
-                onClick={() => navigate(`/viewCode?testName=${encodeURIComponent(selectedRun.testName)}`)}
-              >
-                View Code
-              </button>
-            </div>
-          </div>
-        )}
-      </header>
+      <DashboardHeader selectedRun={selectedRun} onViewCodeClick={handleViewCodeClick} />
       <main className="mt-4 px-4 sm:px-0">
         <section className="w-full h-1/2 flex items-center justify-center bg-gray-800 relative">
           {selectedRun && <video src={selectedRun.videoUrl} controls className="w-full h-full"></video>}
@@ -74,7 +93,7 @@ const TestDashboard = () => {
         <section className="mt-8">
           <h2 className="text-2xl font-bold mb-4">Logs</h2>
           <div className="w-full p-6 bg-gray-800 shadow rounded-lg">
-            <pre><code className="language-markup">{selectedRun && selectedRun.logs.join('\n')}</code></pre>
+            <pre><code className="language-markup">{selectedRun?.logs.join('\n')}</code></pre>
           </div>
         </section>
         <section className="mt-8">
