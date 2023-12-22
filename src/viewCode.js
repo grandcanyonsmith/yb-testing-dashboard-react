a6504fccd4809b2909ea674008c55b656afe8519
Canyon Smith <canyonfsmith@gmail.com>
2023-12-22 22:09:53 +0500

    updated src/viewCode.js

--- src/viewCode.js
+++ src/viewCode.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useReducer } from 'react';
+import { useHistory } from 'react-router-dom'; // Added for history navigation
 import axios from 'axios';
 import Prism from 'prismjs';
 import 'prismjs/themes/prism-okaidia.css';
@@ -8,14 +9,12 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import './ViewCode.css';
 
-// API URLs
 const API_URLS = {
   submit: 'https://kvqpfgxn2jz5pyh4wz7thbmhay0hqcvh.lambda-url.us-west-2.on.aws/',
   execute: 'https://xmichysgq4emm6orafcdnwwhwu0lvmez.lambda-url.us-west-2.on.aws/',
-  fetchFileContents: 'https://zyw4xz6b5m2c7mdbhhsoerydve0mgnfl.lambda-url.us-west-2.on.aws/',
+ fetchFileContents: 'https://zyw4xz6b5m2c7mdbhhsoerydve0mgnfl.lambda-url.us-west-2.on.aws/',
 };
 
-// Loading states
 const LOADING_STATES = {
   idle: 'idle',
   submit: 'submit',
@@ -23,7 +22,6 @@ const LOADING_STATES = {
   fetchFileContents: 'fetchFileContents',
 };
 
-// Initial state
 const initialState = {
   userRequest: '',
   code: '',
@@ -34,7 +32,6 @@ const initialState = {
   error: null,
 };
 
-// Reducer function
 function reducer(state, action) {
   switch (action.type) {
     case 'setUserRequest':
@@ -56,7 +53,6 @@ function reducer(state, action) {
   }
 }
 
-// API request function
 async function handleApiRequest(url, body) {
   try {
     const response = await axios.post(url, body);
@@ -68,6 +64,8 @@ async function handleApiRequest(url, body) {
 
 const ViewCode = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const history = useHistory(); // Used for handling history navigation
+
   const submitUserRequest = async () => {
     dispatch({ type: 'setLoadingState', payload: LOADING_STATES.submit });
     try {
@@ -92,6 +90,7 @@ const ViewCode = () => {
       dispatch({ type: 'setLoadingState', payload: LOADING_STATES.idle });
     }
   };
+
   useEffect(() => {
     const filePath = new URL(window.location.href).searchParams.get('testName');
     if (filePath) {
@@ -101,11 +100,11 @@ const ViewCode = () => {
       console.error('Test name not provided in the URL');
     }
   }, []);
-  
+
   const fetchFileContents = async () => {
     dispatch({ type: 'setLoadingState', payload: LOADING_STATES.fetchFileContents });
     try {
-      const data = await handleApiRequest(API_URLS.fetchFileContents, { filePath: 'tests/publishing/media_versioning/test_yearbook_drafts.py', requestType: 'FETCH_FILE_CONTENTS', branchName: 'main' });
+      const data = await handleApiRequest(API_URLS.fetchFileContents, { filePath: state.testName, requestType: 'FETCH_FILE_CONTENTS', branchName: 'main' });
       dispatch({ type: 'setCode', payload: data });
     } catch (error) {
       dispatch({ type: 'setError', payload: error });
@@ -130,20 +129,24 @@ const ViewCode = () => {
     dispatch({ type: 'setUserRequest', payload: e.target.value });
   };
 
+  const navigateBack = () => {
+    history.goBack(); // Function to navigate back
+  };
+
   return (
     <div className="ViewCode">
-    <a href="#" className="text-sm font-semibold leading-7" style={{position: 'absolute', top: '0', left: '10px', color: 'white'}}>
-      <span aria-hidden="true">&larr;</span> Back
-    </a>
-    
       
-      <div style={{marginTop: '30px'}} className="tabContainer">
+      <button onClick={navigateBack} className="text-sm font-semibold leading-7" style={{ position: 'absolute', top: '0', left: '10px', color: 'white' }}>
+        <span aria-hidden="true">&larr;</span> Back
+      </button>
+      
+      <div style={{ marginTop: '30px' }} className="tabContainer">
         <button onClick={toggleViewCode} className={`tabButton ${state.viewCode ? 'active' : ''}`}>
           Code
-       </button>
+        </button>
         <button onClick={toggleViewCode} className={`tabButton ${!state.viewCode ? 'active' : ''}`}>
           Logs
-       </button>
+        </button>
       </div>
       <div className={`codeContainer ${state.viewCode ? '' : 'hidden'}`}>
         <pre className="line-numbers">
@@ -161,10 +164,10 @@ const ViewCode = () => {
       <div className="inputContainer">
         <textarea value={state.userRequest} onChange={handleUserRequestChange} className="userRequestInput" placeholder="Enter a Request..."></textarea>
         <button onClick={submitUserRequest} className={`submitButton ${state.loadingState === LOADING_STATES.submit ? 'loading' : ''}`} disabled={state.loadingState === LOADING_STATES.submit}>
-            Submit
+          Submit
         </button>
         <button onClick={executeCode} className={`executeButton ${state.loadingState === LOADING_STATES.execute ? 'loading' : ''}`} disabled={state.loadingState === LOADING_STATES.execute}>
-            Execute
+          Execute
         </button>
       </div>
       {state.error && <div className="error">{state.error.message}</div>}
